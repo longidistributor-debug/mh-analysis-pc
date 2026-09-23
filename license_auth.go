4c81d2a37639061d42f44bf882d352004396dd5c
longidistributor-debug <longidistributor@gmail.com>
2026-09-24 01:27:12 +0500

    Bump license_auth.go to V.55.11

--- license_auth.go
+++ license_auth.go
@@ -32,7 +32,7 @@ import (
 )
 
 const licDefaultBaseURL = "https://mh-analysis.vercel.app"
-const licAppVersion = "V.55.10"
+const licAppVersion = "V.55.11"
 
 type licDataBlob struct {
 	cbData uint32
