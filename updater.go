e5ac2b099e978cc4c0e87c4d7449c66383f0ab6d
longidistributor-debug <longidistributor@gmail.com>
2026-09-24 03:21:53 +0500

    Bump updater.go to V.55.15

--- updater.go
+++ updater.go
@@ -21,7 +21,7 @@ import (
 	"time"
 )
 
-const mhPublicVersionV001 = "V.55.14"
+const mhPublicVersionV001 = "V.55.15"
 const mhUpdateManifestURLV001 = "https://raw.githubusercontent.com/longidistributor-debug/mh-analysis-pc/mh-analysis-update-channel/update.json"
 
 type mhUpdateManifestV001 struct {
